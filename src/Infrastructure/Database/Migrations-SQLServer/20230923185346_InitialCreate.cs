cd96de26f738048d64bd2096931df046f179cd05
mikkel.buhl <mikkelnbuhl@gmail.com>
2023-09-23 20:55:31 +0200

    Added SQL Server Initial create migration files

---
+++ src/Infrastructure/Database/Migrations-SQLServer/20230923185346_InitialCreate.cs
@@ -0,0 +1,222 @@
+﻿#if(UseMSSQL)
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ease.Infrastructure.Database.Migrations;
+
+/// <inheritdoc />
+public partial class InitialCreate : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            "Roles",
+            table => new
+            {
+                Id = table.Column<Guid>("uniqueidentifier", nullable: false),
+                Name = table.Column<string>("nvarchar(256)", maxLength: 256, nullable: true),
+                NormalizedName = table.Column<string>("nvarchar(256)", maxLength: 256, nullable: true),
+                ConcurrencyStamp = table.Column<string>("nvarchar(max)", nullable: true)
+            },
+            constraints: table => { table.PrimaryKey("PK_Roles", x => x.Id); });
+
+        migrationBuilder.CreateTable(
+            "Users",
+            table => new
+            {
+                Id = table.Column<Guid>("uniqueidentifier", nullable: false),
+                Name = table.Column<string>("nvarchar(255)", maxLength: 255, nullable: true),
+                Created = table.Column<DateTime>("datetime2", nullable: false),
+                Updated = table.Column<DateTime>("datetime2", nullable: false),
+                UserName = table.Column<string>("nvarchar(256)", maxLength: 256, nullable: true),
+                NormalizedUserName = table.Column<string>("nvarchar(256)", maxLength: 256, nullable: true),
+                Email = table.Column<string>("nvarchar(256)", maxLength: 256, nullable: true),
+                NormalizedEmail = table.Column<string>("nvarchar(256)", maxLength: 256, nullable: true),
+                EmailConfirmed = table.Column<bool>("bit", nullable: false),
+                PasswordHash = table.Column<string>("nvarchar(max)", nullable: true),
+                SecurityStamp = table.Column<string>("nvarchar(max)", nullable: true),
+                ConcurrencyStamp = table.Column<string>("nvarchar(max)", nullable: true),
+                PhoneNumber = table.Column<string>("nvarchar(max)", nullable: true),
+                PhoneNumberConfirmed = table.Column<bool>("bit", nullable: false),
+                TwoFactorEnabled = table.Column<bool>("bit", nullable: false),
+                LockoutEnd = table.Column<DateTimeOffset>("datetimeoffset", nullable: true),
+                LockoutEnabled = table.Column<bool>("bit", nullable: false),
+                AccessFailedCount = table.Column<int>("int", nullable: false)
+            },
+            constraints: table => { table.PrimaryKey("PK_Users", x => x.Id); });
+
+        migrationBuilder.CreateTable(
+            "RoleClaims",
+            table => new
+            {
+                Id = table.Column<int>("int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                RoleId = table.Column<Guid>("uniqueidentifier", nullable: false),
+                ClaimType = table.Column<string>("nvarchar(max)", nullable: true),
+                ClaimValue = table.Column<string>("nvarchar(max)", nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_RoleClaims", x => x.Id);
+                table.ForeignKey(
+                    "FK_RoleClaims_Roles_RoleId",
+                    x => x.RoleId,
+                    "Roles",
+                    "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
+
+        migrationBuilder.CreateTable(
+            "UserClaims",
+            table => new
+            {
+                Id = table.Column<int>("int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                UserId = table.Column<Guid>("uniqueidentifier", nullable: false),
+                ClaimType = table.Column<string>("nvarchar(max)", nullable: true),
+                ClaimValue = table.Column<string>("nvarchar(max)", nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_UserClaims", x => x.Id);
+                table.ForeignKey(
+                    "FK_UserClaims_Users_UserId",
+                    x => x.UserId,
+                    "Users",
+                    "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
+
+        migrationBuilder.CreateTable(
+            "UserLogins",
+            table => new
+            {
+                LoginProvider = table.Column<string>("nvarchar(450)", nullable: false),
+                ProviderKey = table.Column<string>("nvarchar(450)", nullable: false),
+                ProviderDisplayName = table.Column<string>("nvarchar(max)", nullable: true),
+                UserId = table.Column<Guid>("uniqueidentifier", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_UserLogins", x => new { x.LoginProvider, x.ProviderKey });
+                table.ForeignKey(
+                    "FK_UserLogins_Users_UserId",
+                    x => x.UserId,
+                    "Users",
+                    "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
+
+        migrationBuilder.CreateTable(
+            "UserRoles",
+            table => new
+            {
+                UserId = table.Column<Guid>("uniqueidentifier", nullable: false),
+                RoleId = table.Column<Guid>("uniqueidentifier", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_UserRoles", x => new { x.UserId, x.RoleId });
+                table.ForeignKey(
+                    "FK_UserRoles_Roles_RoleId",
+                    x => x.RoleId,
+                    "Roles",
+                    "Id",
+                    onDelete: ReferentialAction.Cascade);
+                table.ForeignKey(
+                    "FK_UserRoles_Users_UserId",
+                    x => x.UserId,
+                    "Users",
+                    "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
+
+        migrationBuilder.CreateTable(
+            "UserTokens",
+            table => new
+            {
+                UserId = table.Column<Guid>("uniqueidentifier", nullable: false),
+                LoginProvider = table.Column<string>("nvarchar(450)", nullable: false),
+                Name = table.Column<string>("nvarchar(450)", nullable: false),
+                Value = table.Column<string>("nvarchar(max)", nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_UserTokens", x => new { x.UserId, x.LoginProvider, x.Name });
+                table.ForeignKey(
+                    "FK_UserTokens_Users_UserId",
+                    x => x.UserId,
+                    "Users",
+                    "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
+
+        migrationBuilder.CreateIndex(
+            "IX_RoleClaims_RoleId",
+            "RoleClaims",
+            "RoleId");
+
+        migrationBuilder.CreateIndex(
+            "RoleNameIndex",
+            "Roles",
+            "NormalizedName",
+            unique: true,
+            filter: "[NormalizedName] IS NOT NULL");
+
+        migrationBuilder.CreateIndex(
+            "IX_UserClaims_UserId",
+            "UserClaims",
+            "UserId");
+
+        migrationBuilder.CreateIndex(
+            "IX_UserLogins_UserId",
+            "UserLogins",
+            "UserId");
+
+        migrationBuilder.CreateIndex(
+            "IX_UserRoles_RoleId",
+            "UserRoles",
+            "RoleId");
+
+        migrationBuilder.CreateIndex(
+            "EmailIndex",
+            "Users",
+            "NormalizedEmail");
+
+        migrationBuilder.CreateIndex(
+            "UserNameIndex",
+            "Users",
+            "NormalizedUserName",
+            unique: true,
+            filter: "[NormalizedUserName] IS NOT NULL");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            "RoleClaims");
+
+        migrationBuilder.DropTable(
+            "UserClaims");
+
+        migrationBuilder.DropTable(
+            "UserLogins");
+
+        migrationBuilder.DropTable(
+            "UserRoles");
+
+        migrationBuilder.DropTable(
+            "UserTokens");
+
+        migrationBuilder.DropTable(
+            "Roles");
+
+        migrationBuilder.DropTable(
+            "Users");
+    }
+}
+#endif
