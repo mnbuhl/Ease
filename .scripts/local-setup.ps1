b4a612a4c0682358e41fb2d4118e72e1f1a76823
mikkel.buhl <mikkelnbuhl@gmail.com>
2023-09-23 18:18:47 +0200

    Add .scripts directory and local setup script
    Added .scripts directory to .template.config/template.json and implemented local-setup.ps1 file to automate the process of cleaning up and reinstalling templates. This change will ease and speed up the development process. Updated exclude list in Ease.nuspec file to ignore .scripts directory.

---
+++ .scripts/local-setup.ps1
@@ -0,0 +1,23 @@
+﻿function Remove-EaseTemplatesNupkg {
+    foreach ($file in Get-ChildItem -Path . -Filter "Ease.Templates.*.nupkg" -Recurse) {
+        Remove-Item $file.FullName
+    }
+}
+
+function Perform-Cleanup {
+    Remove-EaseTemplatesNupkg
+    dotnet new uninstall .\
+    dotnet new uninstall "Ease.Templates"
+}
+
+try {
+    Perform-Cleanup
+    dotnet build -c Release
+    nuget pack Ease.nuspec -NoDefaultExcludes
+    dotnet new install (Get-ChildItem -Path .\ -Filter "Ease.Templates.*.nupkg" | Select-Object -First 1).Name
+    Remove-EaseTemplatesNupkg
+} catch {
+    Write-Host "An error occurred: $_"
+    Perform-Cleanup
+    exit 1
+}
